5f2f4dfb01f6cdb4749ab49a68ac9569f258717f
Kent Gruber <kgruber1@emich.edu>
2016-12-04 02:35:54 -0500

    Update analysis tools, add basic network statistics graph feature

--- controllers/analysis.rb
+++ controllers/analysis.rb
@@ -15,28 +15,65 @@ module Sinatra
             erb :feed
           end
 
+          app.get '/statistics' do
+            redirect to('/') unless is_authenticated?
+            #binding.pry
+            erb :statistics
+          end
+
+          app.post '/statistics' do
+            redirect to('/') unless is_authenticated?
+            if params[:file]
+              if File.extname(params[:file][:filename]).downcase == ".pcap"
+                @stats = Hash.new(0) 
+                PacketFu::PcapFile.read_packets(params[:file][:tempfile]) do |packet|
+                  @stats[packet.proto.last.to_sym] += 1
+                end
+              else
+                @failed = "Tried to upload a non-pcap file."
+              end
+            else
+              @failed = "No file given to upload."
+            end
+            #binding.pry
+            erb :statistics
+          end
+
           app.get '/capture' do
             redirect to('/') unless is_authenticated?
             iface = PacketFu::Utils.default_int
             unless session[:capture_file]
-              session[:capture_file] = PacketFu::Capture.new
+              iface = PacketFu::Utils.default_int
+              session[:capture_file] = PacketFu::Capture.new(:iface => iface)
             end
-            @cap = session[:capture_file]
-            binding.pry
             erb :capture
           end
 
           app.post '/capture/filter' do
             redirect to('/') unless is_authenticated?
-            # apply filter
             binding.pry
-            @cap = session[:capture_file]
+            unless params["iface"].empty?
+              begin
+                session[:capture_file] = PacketFu::Capture.new(:iface => params["iface"])
+              rescue
+                @failed = "Unable to set network face."
+              end
+            end
+
+            unless params["bpf"].empty?
+              begin
+                binding.pry
+                session[:capture_file].bpf(:filter => params["bpf"])
+              rescue
+                binding.pry
+                @failed = "Invalid Berkely Packet Filter Syntax"
+              end
+            end
+            binding.pry
             erb :capture
           end
-
-
-
         end
+
       end
     end
   end
