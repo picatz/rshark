ebdc7b75b1042b95fa0d4d97124ad1edf5aa4209
Kent Gruber <kgruber1@emich.edu>
2016-12-04 05:18:45 -0500

    Update application

--- controllers/analysis.rb
+++ controllers/analysis.rb
@@ -9,9 +9,9 @@ module Sinatra
             iface = PacketFu::Utils.default_int
             cap = PacketFu::Capture.new(:iface => iface)
             cap.start
-            sleep 2
+            sleep 1
             cap.save
-            @capture_sample = cap.array.sample(4)
+            @capture_sample = cap.array
             erb :feed
           end
 
@@ -25,9 +25,23 @@ module Sinatra
             redirect to('/') unless is_authenticated?
             if params[:file]
               if File.extname(params[:file][:filename]).downcase == ".pcap"
-                @stats = Hash.new(0) 
+                @protocol_stats = Hash.new(0) 
+                @mac_addr_stats = Hash.new(0) 
+                @ip_addr_stats  = Hash.new(0) 
                 PacketFu::PcapFile.read_packets(params[:file][:tempfile]) do |packet|
-                  @stats[packet.proto.last.to_sym] += 1
+                  @protocol_stats[packet.proto.last.to_sym] += 1
+                  begin
+                    @mac_addr_stats[packet.eth_daddr] += 1
+                    @mac_addr_stats[packet.eth_saddr] += 1
+                  rescue
+                    # :(
+                  end
+                  begin
+                    @ip_addr_stats[packet.ip_saddr]   += 1 
+                    @ip_addr_stats[packet.ip_daddr]   += 1
+                  rescue
+                    # :(
+                  end
                 end
               else
                 @failed = "Tried to upload a non-pcap file."
@@ -35,7 +49,6 @@ module Sinatra
             else
               @failed = "No file given to upload."
             end
-            #binding.pry
             erb :statistics
           end
 
